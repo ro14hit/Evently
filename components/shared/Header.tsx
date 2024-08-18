4d58f86af626562f6bedad4063372449bac116d1
ro14hit <rohit1499singh@gmail.com>
2024-08-18 17:51:41 +0530

    First Commit with Header Component

---
+++ components/shared/Header.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='w-full border-b'>
+      <div className='wrapper flex items-center justify-between '>
+        <Link href="/" className='w-36'>
+        <Image alt="Evently Logo" width={128} height={38} src="/assets/images/logo.svg"/>
+        </Link>
+        <div className='flex w-32 justify-end gap-3'></div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
